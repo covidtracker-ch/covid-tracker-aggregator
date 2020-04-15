2ce04152da4b3626ba269187e58a59389a3ff766
Luca Naterop <lucanaterop@Lucas-Mac-mini-2.local>
2020-04-15 22:57:35 +0200

    post summary

--- server/postData.js
+++ server/postData.js
@@ -5,42 +5,43 @@ import Zips from '/imports/api/zips';
 import ZipsDaily from '/imports/api/zipsDaily';
 import ZipsWeekly from '/imports/api/zipsWeekly';
 
-function postData() {
-  console.log('post data to dataserver...')
-  const zips = Zips.find({submissions: {$gte: 50}}).fetch();
+function getSummary(zips) {
   let cases = 0;
   let submissions = 0;
   zips.forEach(z => {
     cases += z.cases;
     submissions += z.submissions;
-  })
+  });
+  return {cases, submissions}
+}
 
+function postData() {
+  console.log('post data to dataserver...')
 
-  HTTP.post(Meteor.settings.postURL,{
-    data: {
-      zips: zips,
-      zipsDaily: ZipsDaily.find({submissions: {$gte: 50}}).fetch(),
-      zipsWeekly: ZipsWeekly.find({submissions: {$gte: 50}}).fetch(),
-      total: {
-        cases, submissions
-      }
+  let postObj = {
+    zips: Zips.find({submissions: {$gte: 50}}).fetch(),
+    zipsDaily: ZipsDaily.find({submissions: {$gte: 50}}).fetch(),
+    zipsWeekly: ZipsWeekly.find({submissions: {$gte: 50}}).fetch(),
+    summary: {
+      total: getSummary(Zips.find().fetch()),
+      daily: getSummary(ZipsDaily.find().fetch()),
+      weekly: getSummary(ZipsWeekly.find().fetch())
     }
-  }, r => {
-    // console.log(r)
-  })
+  }
 
-  HTTP.post(Meteor.settings.postURL2,{
-    data: {
-      zips: zips,
-      zipsDaily: ZipsDaily.find({submissions: {$gte: 50}}).fetch(),
-      zipsWeekly: ZipsWeekly.find({submissions: {$gte: 50}}).fetch(),
-      total: {
-        cases, submissions
-      }
-    }
-  }, r => {
-    // console.log(r)
-  })
+  console.log(postObj);
+
+  // HTTP.post(Meteor.settings.postURL,{
+  //   data: postObj
+  // }, r => {
+  //   // console.log(r)
+  // })
+
+  // HTTP.post(Meteor.settings.postURL2,{
+  //   data: postObj
+  // }, r => {
+  //   // console.log(r)
+  // })
   console.log('done');
 }
 
